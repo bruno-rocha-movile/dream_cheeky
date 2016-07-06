570c04b5222de21b2d0003c2d4a0e7bd4def2818
bruno-rocha-movile <bruno.rocha@movile.com>
2016-07-06 14:58:05 -0300

    syntax

--- examples/sendMessageToSlack.py
+++ examples/sendMessageToSlack.py
@@ -1,7 +1,7 @@
 from slacker import Slacker
 
-def asd():
+def sendMessage():
     slack = Slacker('xoxb-57348209414-Jc4RwqzA8i0Ue6HNhRUJmkQi')
     slack.chat.post_message('#bruno-bot-test', 'beep boop')
 
-asd()
+sendMessage()
